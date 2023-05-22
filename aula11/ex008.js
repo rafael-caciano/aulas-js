1da4303332e4cb8b98153eb4ad28594858675152
Rcaciano <128509918+Rcaciano@users.noreply.github.com>
2023-05-22 13:57:29 -0300

    aula 10

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`Sua velocidade é de ${vel}km/h`)
+
+if(vel > 60){
+    console.log('Você ultrapassou o limite de velocidade, MULTADO!')
+}
+
+console.log(`Dirija com cuidado e use sempre o cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+console.log(`Se o seu pais é ${pais}`)
+
+if(pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+} else{
+    console.log('Você é Estrangeiro')
+}
