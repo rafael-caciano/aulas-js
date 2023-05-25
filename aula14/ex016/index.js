5733d0268b30b6d9348a3e67677c95bf2f9b5f56
Rcaciano <128509918+Rcaciano@users.noreply.github.com>
2023-05-25 00:58:52 -0300

    resolução do guanabara para o exercicio

--- aula14/ex016/index.js
+++ aula14/ex016/index.js
@@ -1,4 +1,4 @@
-function contar(){
+/*function contar(){
     var ini = document.getElementById('inicio')
     var fim = document.getElementById('fim')
     var pass= document.querySelector('input#passo')
@@ -13,9 +13,45 @@ function contar(){
     
         res.innerHTML += `${i}`
         i+=p
-    }
-    
-    
+    }  
+}*/
 
-    
+function contar(){
+    //defininco variaveis 
+    let ini = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let pass= document.querySelector('input#passo')
+    let resultado = document.getElementById('res')
+
+
+
+    if(ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0){
+        res.innerHTML = 'Impossivel Contar'
+    }else{
+        resultado.innerHTML = `contando... <br>`
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pass.value)
+
+        if (p <= 0 ) {
+            window.alert('Passo inválido! Conssiderando Passo valendo 1')
+            p = 1
+        }
+
+    //condição feita dentro do "else" do primeiro "if"
+
+    if(i < f){
+        //Contagem progressiva
+        for(let c = i; c <= f; c += p ){
+            res.innerHTML += `${c} \u{1f449}`
+        }
+    }else{
+        //contagem regressiva
+        for(let c = i; c >= f; c -= p){
+            res.innerHTML += `${c} \u{1f449}`
+        }
+        res.innerHTML += `\u{1f3c1}`
+    }
+      
+    }
 }
--- aula14/ex017/index.js
+++ aula14/ex017/index.js
@@ -1,1 +1,17 @@
-alert('olá!!')
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if(num.value.length == 0){
+        window.alert('Por favor digite um numero')
+    }else{
+        let n = Number(num.value)
+        let c = 1
+
+        while (c <= 10 ){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+    }
+    }
+}
