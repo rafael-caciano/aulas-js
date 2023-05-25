b56e6bb9fe67f8599524b22999477060b9bbf270
Rcaciano <128509918+Rcaciano@users.noreply.github.com>
2023-05-24 22:01:29 -0300

    tentei fazer o codgo js sozinho

--- aula14/ex016/index.js
+++ aula14/ex016/index.js
@@ -1,11 +1,21 @@
 function contar(){
     var ini = document.getElementById('inicio')
     var fim = document.getElementById('fim')
-    var pass= document.getElementsByName('passo')
+    var pass= document.querySelector('input#passo')
     var resultado = document.getElementById('res')
     var i = Number(ini.value)
     var f = Number(fim.value)
     var p = Number(pass.value)
+
+
+    
+    while(i <= f){
+    
+        res.innerHTML += `${i}`
+        i+=p
+    }
+    
+    
 
     
 }
