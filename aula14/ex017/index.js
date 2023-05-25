abdbd49253e8f0d92b4714a58a4ad45a83608960
Rcaciano <128509918+Rcaciano@users.noreply.github.com>
2023-05-25 01:52:25 -0300

    resolução guanabara para o segundo exercicio

--- aula14/ex017/index.js
+++ aula14/ex017/index.js
@@ -7,11 +7,13 @@ function tabuada(){
     }else{
         let n = Number(num.value)
         let c = 1
+        tab.innerHTML = ''
 
-        while (c <= 10 ){
+        while(c <= 10){
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             tab.appendChild(item)
-    }
+            c++
+        }
     }
 }
