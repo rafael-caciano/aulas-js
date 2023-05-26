f1687ac0602c314dcb71a63b90de3a523000e315
Rcaciano <128509918+Rcaciano@users.noreply.github.com>
2023-05-26 00:54:01 -0300

    tentei fazer sozinho o exercicio

--- aula17/modelo/index.js
+++ aula17/modelo/index.js
@@ -6,16 +6,35 @@
 //caso tenha mostrar quantos elementos temos usando o .length
 //qual é o maior e o menor numero, ordenando eles com o .sort e mostrando o primeiro e o ultimo valor
 //somar todos os valores e depois dividilos pelo numero de elementos
+    
+
+let num = window.document.getElementById('fnum')
+    let lista = document.getElementById('flista')
+    let res = document.getElementById('res')
+    let valores = []
+
 
 function adicionar(){
-    let num = window.document.getElementById('txtn')
     
-    if(num.value.length == 0){
+    if(num.value.length == 0 ){
         window.alert('Preencha o campo')
+    }else if( num.value < 1 || num.value > 100){
+        window.alert('Numero inválido')
     }else{
-        let n = Number(num.value)
-        
+
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text= `Valor ${num.value} foi cadastrado`
+        lista.appendChild(item)
     }
+}
 
-
+function finalizar(){
+  
+       if(valores.indexOf() != -1){ 
+        window.alert('Não existem valores cadastrados')
+    }else{
+        window.alert('ok ok ok')
+    }
+   
 }
